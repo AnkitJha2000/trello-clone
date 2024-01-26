e7b32e63e5b33629e0475af7eae5ea5b6131e00c
AnkitJha2000 <ankitsakshi9800@gmail.com>
2024-01-26 16:52:41 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const { Console } = require('console')
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/') res.end("welcome to our server")
+    if(req.url === '/about') res.end("here is the about section of our page")
+    res.end(
+        `<h1>4 04 : NOT FOUND </h1> 
+        <p>OOPPS! There was an error</p> 
+        <a href='/'> back home</a>`
+    )
+})
+
+server.listen(5000)
